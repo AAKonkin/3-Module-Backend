66b3d8b03a18d39798311b94f3e673edfe9c310e
AAKonkin <konich1999@yandex.ru>
2025-06-26 10:12:29 +0300

    something wrong...trying to fix

--- 3 Modules/3_Path_Module/path.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-console.log(path.dirname(__filename));
-console.log(path.basename(__filename));
-console.log(path.extname(__filename));
-console.log(path.extname(__filename).slice(1));
-console.log(path.parse(__filename));
-console.log(path.resolve(__dirname, "..", "./1_ES_Modules", "./app.js"));
-console.log(path.join(__dirname, "..", "./1_ES_Modules", "./app.js"));
